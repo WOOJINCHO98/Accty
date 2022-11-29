4d4b3e49af4d735796f070b3c38176bfe5ab4372
WOOJINCHO98 <steamedbunwith@gmail.com>
2022-11-30 04:26:07 +0900

    just commit

--- app/src/main/java/com/example/acther/SurfingActivity.java
+++ app/src/main/java/com/example/acther/SurfingActivity.java
@@ -26,7 +26,7 @@ public class SurfingActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_golf);
+        setContentView(R.layout.activity_surfing);
 
 
 
